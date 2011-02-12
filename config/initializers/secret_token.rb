6591ade926468bd6a387a940a87c38d5760bbabe
root <root@fedoraVM.(none)>
2011-02-13 00:14:33 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '61f93195f76ade9ca1310673a0f0ecb090560b40197073f687c06150ebd8814b131051bbb6b377b8b0eede4b98af08373e8130b1fa831e5f2df2cac0ad48bb8d'
